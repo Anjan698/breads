95ecfbb536718e4ce7716f19d69a15e2986c3ce2
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-20 21:19:49 -0400

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,8 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+
+
 // INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
@@ -12,7 +14,9 @@ breads.get('/', (req, res) => {
   // res.send(Bread)
 })
 
-
+function Show({bread,index}){
+  
+}
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
--- views/index.jsx
+++ views/index.jsx
@@ -1,40 +1,23 @@
-const React = require('react')
-const Default = require('./layouts/Default')
-function Show ({bread, index}) {
+const React = require("react");
+const Default = require("./layouts/Default");
 
-function Index ({breads})  {
-    return (
-        <Default>
-            
-<form action={`/breads/${index}?_method=DELETE`} method="POST">
-  <input type='submit' value="DELETE"/>
-</form>
-
-        <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        {/* This is a JSX comment. */}
-        <ul>
-         {
-    breads.map((bread, index)=> {
-      return (
-        <li key={index}>
-          <a href={`/breads/${index}`}>
-            {bread.name}
-          </a>
-        </li>
-        
-      )
-    })
-  }
-  
-  
-        </ul>
-      </Default>
-      
-      
-    
-    )
+function Index({ breads }) {
+  return (
+    <Default>
+      <h2>Index Page</h2>
+      {/* <p>I have {breads[0].name} bread!</p> */}
+      {/* This is a JSX comment. */}
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${index}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </Default>
+  );
 }
 
-
-module.exports = Index
+module.exports = Index;
