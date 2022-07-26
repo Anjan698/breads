4018e7edb24232e9229253ed2b33fea754e1b77f
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-25 21:23:05 -0400

    Its Working

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,54 +1,50 @@
-const express = require('express')
-const breads = express.Router()
-const Bread = require('../models/bread.js')
+const express = require("express");
+const breads = express.Router();
+const Bread = require("../models/bread.js");
 
 // INDEX
-breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread
-      }
-    )
+breads.get("/", (req, res) => {
+  res.render("Index", {
+    breads: Bread,
+  });
   // res.send(Bread)
-})
+});
 
-function Show({bread,index}){
-  
-}
+function Show({ bread, index }) {}
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
+breads.get("/:arrayIndex", (req, res) => {
   if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
       index: req.params.arrayIndex,
-    })
+    });
   } else {
-    res.render('404')
+    res.render("404");
   }
-})
+});
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
+breads.get("/:indexArray/edit", (req, res) => {
+  res.render("edit", {
     bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
-})
+    index: req.params.indexArray,
+  });
+});
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-  if(req.body.hasGluten === 'on'){
-    req.body.hasGluten = true
+breads.put("/:arrayIndex", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
   } else {
-    req.body.hasGluten = false
+    req.body.hasGluten = false;
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
-})
+  Bread[req.params.arrayIndex] = req.body;
+  res.redirect(`/breads/${req.params.arrayIndex}`);
+});
 
-   //DELETE
-   breads.delete('/:indexArray', (req,res) => {
-    Bread.splice(req.params.indexArray, 1)
-    res.status(303).redirect('/breads')
-   })
-  
-module.exports= breads
+//DELETE
+breads.delete("/:indexArray", (req, res) => {
+  Bread.splice(req.params.indexArray, 1);
+  res.status(303).redirect("/breads");
+});
+
+module.exports = breads;
